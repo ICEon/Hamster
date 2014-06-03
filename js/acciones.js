805ec76b4b0df2f9b3e6988fc888b69bf700047c
Eon Matrix <matrix_eon@yahoo.com>
2014-06-03 09:47:10 -0500

    ocho

--- js/acciones.js
+++ js/acciones.js
@@ -6,6 +6,68 @@ $(document).ready(function(e) {
     navigator.notification.vibrate(500);
   });
  },false); //deviceready
+ 
+  $('#logo img').tap(function(e){	
+	
+	
+	
+    $('#logo img').toggleClass('clicked');
+	$('#vista img').toggleClass('clicked');
+	
+});//click 
+
+
+
+    $('#vista img').tap(function(e){	
+    $('#vista img').toggleClass('clicked');
+	$('#ham img').toggleClass('clicked');
+ $.mobile.navigate( "#principal" );	
+});//click 
+
+    //$('#vista img').tap(function(e){	
+	// $("#roar")[0].play();	
+	// $.mobile.navigate( "#principal" );
+	//});
+	
+	$('#vida').tap(function (e) {
+ 	 $.mobile.navigate( "#ray1" );
+	});
+
+
+$('#enfer').tap(function (e) {
+ 	 $.mobile.navigate( "#enf" );
+	});
+
+
+
+
+$('#cuidar').tap(function (e) {
+ 	 $.mobile.navigate( "#c" );
+	
+	});
+	
+	
+$('#tip').tap(function (e) {
+ 	 $.mobile.navigate( "#tipos" );
+	
+	});
+	
+	
+	$('#domo').tap(function (e) {
+ 	 $.mobile.navigate( "#domo1" );
+	
+	});
+	
+	
+	$('#acerca').tap(function (e) {
+		
+ 	 $.mobile.navigate( "#de" );
+	
+	});
+	
+	
+	
+ 
 }); //document ready
 
 
