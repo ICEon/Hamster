26030f93318017f65f36bd2f910946ad53936955
Eon Matrix <matrix_eon@yahoo.com>
2014-06-03 08:47:35 -0500

    tres

--- js/acciones.js
+++ js/acciones.js
@@ -9,65 +9,6 @@
 },false); //deviceready
 
 
-    $('#logo img').tap(function(e){	
-	
-	
-	
-    $('#logo img').toggleClass('clicked');
-	$('#vista img').toggleClass('clicked');
-	
-});//click 
-
-
-
-    $('#vista img').tap(function(e){	
-    $('#vista img').toggleClass('clicked');
-	$('#ham img').toggleClass('clicked');
- $.mobile.navigate( "#principal" );	
-});//click 
-
-    //$('#vista img').tap(function(e){	
-	// $("#roar")[0].play();	
-	// $.mobile.navigate( "#principal" );
-	//});
-	
-	$('#vida').tap(function (e) {
- 	 $.mobile.navigate( "#ray1" );
-	});
-
-
-$('#enfer').tap(function (e) {
- 	 $.mobile.navigate( "#enf" );
-	});
-
-
-
-
-$('#cuidar').tap(function (e) {
- 	 $.mobile.navigate( "#c" );
-	
-	});
-	
-	
-$('#tip').tap(function (e) {
- 	 $.mobile.navigate( "#tipos" );
-	
-	});
-	
-	
-	$('#domo').tap(function (e) {
- 	 $.mobile.navigate( "#domo1" );
-	
-	});
-	
-	
-	$('#acerca').tap(function (e) {
-		
- 	 $.mobile.navigate( "#de" );
-	
-	});
-	
-	
-	
+   
 	 
 });//ready
